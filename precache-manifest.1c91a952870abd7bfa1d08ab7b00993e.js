f352807307e84c52cf9d20bb399980f689d73384
David Yun <yunpro@Davids-MacBook-Pro.local>
2019-07-30 10:10:56 -0500

    Updates

---
+++ precache-manifest.1c91a952870abd7bfa1d08ab7b00993e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd38f907469614e94d4474ee6bdf306",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5fd717552598e6f63a55",
+    "url": "/portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8c122d85f91d73667028",
+    "url": "/portfolio/static/css/main.fc1d7ec8.chunk.css"
+  },
+  {
+    "revision": "5fd717552598e6f63a55",
+    "url": "/portfolio/static/js/2.15f2ae3e.chunk.js"
+  },
+  {
+    "revision": "8c122d85f91d73667028",
+    "url": "/portfolio/static/js/main.93a7cdfe.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d0b42812787fe2030344b986c49b3b02",
+    "url": "/portfolio/static/media/chatproject.d0b42812.png"
+  },
+  {
+    "revision": "01de7577e2445a7a7c62d037415a10ec",
+    "url": "/portfolio/static/media/nasa.01de7577.png"
+  },
+  {
+    "revision": "bddc7d8a0e9abaaf754a0e72c49dda01",
+    "url": "/portfolio/static/media/profile.bddc7d8a.jpg"
+  },
+  {
+    "revision": "aa8559985aa9b5b46b589af3d81d5bf9",
+    "url": "/portfolio/static/media/super.aa855998.png"
+  }
+]);
